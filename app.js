dd30d79d5b7bb54a7251cacd09e165c8150060e7
boscarinomarcelo <102967743+boscarinomarcelo@users.noreply.github.com>
2022-04-27 10:57:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const getData1 = async(simbolo,moneda) =>{
+    
+    const endpoint = `https://api.covalenthq.com/v1/pricing/tickers/?quote-currency=${moneda}&format=JSON&tickers=${simbolo}&key=ckey_21be25f5c33d41c58c22cf020ab`
+    
+    const resp = await fetch(endpoint)
+    const json = await resp.json()
+    datos = json.data.items
+    if(datos.length>0) {
+    const tabla = document.getElementById('tabla') 
+    let row = document.createElement('tr');
+    let row_data_1 = document.createElement('td');
+    let row_data_2 = document.createElement('td');
+    let row_data_3 = document.createElement('td');
+    let row_data_4 = document.createElement('td');
+    const datosTabla = datos.map(data =>
+        {
+          row_data_1.innerHTML = `<img src=${data.logo_url} height="30">`
+          row_data_2.innerHTML = data.contract_ticker_symbol
+          row_data_3.innerHTML = data.contract_name
+          row_data_4.innerHTML = `${data.quote_rate} ${moneda}`
+        }
+      )
+    row.appendChild(row_data_1);
+    row.appendChild(row_data_2);
+    row.appendChild(row_data_3);
+    row.appendChild(row_data_4);
+    tabla.appendChild(row);
+      }
+      else{window.alert('No Existe el Símbolo en API Covalent')}
+}
+function agregar(){
+let i=crypto_mendas.length
+if (document.getElementById('simboloCripto').value.trim() !=""){
+crypto_mendas[i]=document.getElementById('simboloCripto').value
+document.getElementById('simboloCripto').value = ''
+let moneda='USD'
+if (document.formulario.options[0].checked){
+  moneda='USD'
+}
+else {  
+  if(document.formulario.options[1].checked){
+      moneda='ARS'
+  }
+  else{ moneda='EUR'}
+}
+
+getData1(crypto_mendas[i], moneda)
+}}
+
+function refrescar(){
+location.reload()
+}
+
+let datos
+const crypto_mendas = ['btc','eth','usdt', 'usdc']
+
+for(let i=0;i < crypto_mendas.length;i++)
+{
+  getData1(crypto_mendas[i],'USD')
+}
+
